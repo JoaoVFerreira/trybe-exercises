18306ada6f0db1fd15ec7b48c27a35cf885add50
João Ferreira <joaovtf39@gmail.com>
2021-06-24 22:47:33 -0300

    Exercicios de JS - dia 2

---
+++ Fundamentals/block_4/4.2/javascript-day2.js
@@ -0,0 +1,73 @@
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers)*/
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+    soma = soma + numbers[i];
+}
+
+console.log(soma); */
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let quantidade = numbers.length
+let media = 0
+
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i+= 1){
+   
+    soma = soma + numbers[i];
+    media = soma/quantidade
+}
+
+console.log(media)
+
+if (media > 20) {
+    console.log('valor maior que 20')
+} else {
+    console.log('valor menor ou igual a 20')
+}
+*/
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero = numbers[0]
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > maiorNumero){
+        maiorNumero = numbers[i]
+    }
+    
+}
+console.log (maiorNumero)*/
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let quantidadePar = 0;
+
+for (let i = 0; i < numbers.length; i += 1)
+if ( numbers[i] % 2 === 0) {
+    quantidadePar += 1
+} else {
+    quantidadeImpar = numbers.length - quantidadePar
+}
+
+console.log(quantidadeImpar)*/
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let MenorNumero = numbers[0]
+
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < MenorNumero) {
+        MenorNumero = numbers[i]
+    }
+
+}
+
+console.log(MenorNumero)*/
+
