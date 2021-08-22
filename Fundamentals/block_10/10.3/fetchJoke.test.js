34d853a3523d5fa628b5730cea9beb6b23a70aa9
João Ferreira <joaovtf39@gmail.com>
2021-08-22 00:31:34 -0300

    ok

--- Fundamentals/block_10/10.3/fetchJoke.test.js
+++ Fundamentals/block_10/10.3/fetchJoke.test.js
@@ -6,40 +6,18 @@ const fetchJoke = () => {
     .then(data => data.joke);
 };
 
-const fetch = jest.fn(() => {
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
+const fetch = jest.fn(async () => {
+  return {
+    json: async () => {
+      return {
         id: '7h3oGtrOfxc',
         joke: 'Whiteboards ... are remarkable.',
         status: 200
-      })
-  })
+      }
+    }
+  }
 })
 
-it('should work fine', () => {
-  return expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
-})
-
-/*const API_URL = 'https://icanhazdadjoke.com/';
-
-const fetchJoke = () => {
-  return fetch(API_URL, { headers: { Accept: "application/json" } })
-    .then(response => response.json())
-    .then(data => data.joke);
-};
-
-const fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        id: "7h3oGtrOfxc",
-        joke: "Whiteboards ... are remarkable.",
-        status: 200
-      })
-  })
-);
-
-test("espera-se que o fetch retorne uma piada", () =>
-  expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
-); */
+it('should work fine', async () => {
+  await expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
+});
